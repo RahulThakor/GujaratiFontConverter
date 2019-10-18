d67d0eaeedc35c5928bf6605023844bd44661c5d
Rahul A. Thakor <46476618+RahulThakor@users.noreply.github.com>
2019-10-18 23:37:14 +0530

    update copy aleart and remove version

--- js/convertsys.js
+++ js/convertsys.js
@@ -43,7 +43,6 @@ function copy2clip(id) {
     copyText.select();
     copyText.setSelectionRange(0, 99999)
     document.execCommand("copy");
-    alert("Copied the text: " + copyText.value);
   }
 
  
