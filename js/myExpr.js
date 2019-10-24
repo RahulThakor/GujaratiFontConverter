3532977c24cabe480afdac61cbb4b9072fcab615
Thakor <thakorrahul5@gmail.com>
2019-10-24 12:17:02 +0530

    r

--- js/myExpr.js
+++ js/myExpr.js
@@ -359,9 +359,12 @@ function UnicodeToGopika(isArpita) {
    
    // ã = long  િ  --> following code to be corrected accordingly
    
-   modified_substring = modified_substring.replace( /([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([Š])/g , "$2$1" ) ;
+   modified_substring = modified_substring.replace( /([ક્ ખ્ ગ્ ઘ્ ઝ઼ ચ્ જ્ ઞ્ ણ્ ત્ થ્ ધ્ ન્ પ્ ફ્ બ્ ભ્ મ્ ય્ લ્ વ્ શ્ સ્ ષ્ હ્ ળ્])([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([Š])/g , "$3$1$2" ) ;
 
-   modified_substring = modified_substring.replace( /([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([r])/g , "$2$1" ) ;
+   //modified_substring = modified_substring.replace( /([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([Š])/g , "$2$1" ) ;
+
+   modified_substring = modified_substring.replace( /([ક્ ખ્ ગ્ ઘ્ ઝ઼ ચ્ જ્ ઞ્ ણ્ ત્ થ્ ધ્ ન્ પ્ ફ્ બ્ ભ્ મ્ ય્ લ્ વ્ શ્ સ્ ષ્ હ્ ળ્])([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([r])/g , "$3$1$2" ) ;
+   //modified_substring = modified_substring.replace( /([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([r])/g , "$2$1" ) ;
 
    modified_substring = modified_substring.replace( /([કખગઘઙચછજઝઞટઠડઢણતથદધનપફબભમયરલવશષસહળ])([®])/g , "$2$1" ) ;
    
